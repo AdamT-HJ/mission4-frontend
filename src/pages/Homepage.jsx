80a1de5540f8b88bb419698fef3f75a7b3a955f5
AdamT <adamt@missionreadyhq.com>
2025-06-09 11:40:59 +1200

    first commit basic frontend setup

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Homepage.module.css'
+
+export default function Homepage() {
+    <h1></h1>
+  return (
+    <>
+        <div>Homepage</div>
+        <h1></h1>
+    
+    
+    </>
+  )
+}
